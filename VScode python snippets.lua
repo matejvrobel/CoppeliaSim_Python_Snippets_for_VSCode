e61aee595697a36a5eceac676718534caff77a71
matejvrobel <matejvrobel@gmail.com>
2025-09-28 10:57:23 +0200

    Update VScode python snippets.lua

--- VScode python snippets.lua
+++ VScode python snippets.lua
@@ -23,6 +23,20 @@ function sysCall_init()
             s = string.gsub(s, "\n","")
             local ep = string.find(s, ")")
             if ep then
+                --float[], float[3], float[3..9], float[3..*] -> list
+                s = string.gsub(s, "float%[[^%]]*%]", "list")
+                s = string.gsub(s, "int%[[^%]]*%]", "list")
+                s = string.gsub(s, "any%[[^%]]*%]", "list")
+                s = string.gsub(s, "string%[[^%]]*%]", "list")
+                s = string.gsub(s, "map%[[^%]]*%]", "list")
+
+                s = string.gsub(s, "map", "dict")
+                s = string.gsub(s, "string", "str")
+                s = string.gsub(s, "nil", "None")
+                s = string.gsub(s, "true", "True")
+                s = string.gsub(s, "false", "False")
+                s = string.gsub(s, '"', '\\"')
+
                 s = string.sub(s,1,ep-1)
                 ep = string.find(s,"%(")
                 if ep then
@@ -30,25 +44,23 @@ function sysCall_init()
                     local params = {}
                     local params_dec = {}
                     local idx = 1
+
+                    local pres = ""
+                    if ep then pres = string.sub(s, 0, ep - 1) end
+                    
                     for param in string.gmatch(args,"([^,]+)") do
                         if not string.find(param, "=") then
                             table.insert(params,"$"..idx)
                             idx = idx + 1
                         end
                         param = string.gsub(param,"^%s*(.-)%s*$","%1")
-                        param = string.gsub(param, "float%[%d+%]", "list")
-                        param = string.gsub(param, "int%[%d+%]", "list")
-                        param = string.gsub(param, "map", "dict")
-                        param = string.gsub(param, "string", "str")
-                        param = string.gsub(param, "nil", "None")
-                        param = string.gsub(param, '"', '\\"')
                         table.insert(params_dec, param)
                     end
                     local bodyStr = funcName.."("..table.concat(params,", ")..")"
                     finalTxt = finalTxt..'  "'..funcName..'": {\n'
                     finalTxt = finalTxt..'    "prefix": "'..funcName..'",\n'
                     finalTxt = finalTxt..'    "body": ["'..bodyStr..'"],\n'
-                    finalTxt = finalTxt..'    "description": "'..funcName..'('..table.concat(params_dec,", ")..')"\n'
+                    finalTxt = finalTxt..'    "description": "'..pres..'('..table.concat(params_dec,", ")..')"\n'
                     finalTxt = finalTxt..'  }'
                     if i<#funcs then finalTxt = finalTxt..',' end
                     finalTxt = finalTxt..'\n'
@@ -72,10 +84,10 @@ function sysCall_init()
 
     finalTxt = finalTxt.."}\n"
 
-    local file = io.open("coppeliasim_snippets.code-snippets","w")
+    local file = io.open("coppeliasim_py.code-snippets","w")
     file:write(finalTxt)
     io.close(file)
-    sim.addLog(sim.verbosity_msgs,"Wrote 'coppeliasim_snippets.code-snippets'")
+    sim.addLog(sim.verbosity_msgs,"Wrote 'coppeliasim_py.code-snippets'")
 
     return {cmd='cleanup'}
 end
